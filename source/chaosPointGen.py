dbebbbc4fd0728f1bf66f9c91ef08cc925b32318
Anton <antonvc@gmail.com>
2017-09-07 13:25:53 +0800

    create chaosPointGen and generated points

---
+++ source/chaosPointGen.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 13:12:56 2017
+
+@author: ADMIN
+"""
+
+import point_generator as pg
+
+for i in [5,10,20,30,40,60]:
+    pg.pointGen(aStoC, bCtoS, beta, N, M, i, time, t_1, Trials, interval)
