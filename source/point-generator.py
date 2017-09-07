fe9703e4d7e076200a0bdaf6de4834f4ab706383
Anton <antonvc@gmail.com>
2017-09-07 13:01:43 +0800

    make point-generator a function (pointGen)

--- source/point-generator.py
+++ source/point-generator.py
@@ -13,7 +13,7 @@ import numpy as np
 #####Simulation Parameters#####
 N = 10
 M = 10
-startPop = 10
+startPop = 20
 time = 100
 t_1 = 0
 aStoC = 1.0
@@ -24,41 +24,42 @@ beta = 10
 Trials = 50
 interval = 0.02
 ##############################
+def pointGen(aStoC, bCtoS, beta, N, M, startPop, time, t_1, Trials, interval):
+    
+    meanlist = []
+    stdlist = []
+    ilist = []
+    for i in np.arange(0,1.0,interval):
+        list_a = []
+        for j in range(Trials):
+            list_a.append(app.app_sim(aStoC, bCtoS, i, beta, N, M, startPop, time, t_1)[time-1]) #takes the last number from the appsim vector
+        mean = np.mean(np.array(list_a))
+        std = np.std(np.array(list_a))  
+        meanlist.append(mean)
+        stdlist.append(std)
+        ilist.append(i)
+    #    plt.scatter(i,mean, color='k')
+    
+    ilist = np.array(ilist)
+    meanlist = np.array(meanlist)
+    stdlist = np.array(stdlist)
+    
+    #valuesmatrix = np.vstack((ilist,meanlist,stdlist)).T
+    filename = 'b = ' + str(bCtoS) + ', beta = ' + str(beta) + ', startPop = ' + str(startPop) + '.txt'
+    #np.savetxt(filename, valuesmatrix, delimiter = ',')
+    
+    with open(filename,'w') as f:
+        lis=[ilist,meanlist,stdlist]
+        for x in zip(*lis):
+            f.write("{0}\t{1}\t{2}\n".format(*x))
+            
+    return (ilist,meanlist,stdlist)
 
-xgraph = []
-ygraph = []
-meanlist = []
-stdlist = []
-ilist = []
-for i in np.arange(0,1.0,interval):
-    list_a = []
-    for j in range(Trials):
-        list_a.append(app.app_sim(aStoC, bCtoS, i, beta, N, M, startPop, time, t_1)[time-1]) #takes the last number from the appsim vector
-    mean = np.mean(np.array(list_a))
-    std = np.std(np.array(list_a))  
-    meanlist.append(mean)
-    stdlist.append(std)
-    ilist.append(i)
-#    plt.scatter(i,mean, color='k')
 
-ilist = np.array(ilist)
-meanlist = np.array(meanlist)
-stdlist = np.array(stdlist)
-
-#valuesmatrix = np.vstack((ilist,meanlist,stdlist)).T
-filename = 'b = ' + str(bCtoS) + ', beta = ' + str(beta) + '.txt'
-#np.savetxt(filename, valuesmatrix, delimiter = ',')
-
-with open(filename,'w') as f:
-    lis=[ilist,meanlist,stdlist]
-    for x in zip(*lis):
-        f.write("{0}\t{1}\t{2}\n".format(*x))
-
-
-                   
+x = pointGen(aStoC, bCtoS, beta, N, M, startPop, time, t_1, Trials, interval)             
 plt.title('b = ' + str(bCtoS) + ', beta = ' + str(beta) + ', startPop = ' + str(startPop))
 plt.xlabel('a * alpha')
 plt.ylabel('n')
-plt.scatter(ilist,meanlist)
+plt.scatter(x[0],x[1])
 plt.show()
 
