c01ea0899d0a7ae9b6504266096c91356cf38017
Anton <antonvc@gmail.com>
2018-01-27 18:54:55 +0800

    add comments per function to applause_functions

--- source/applause_functions.py
+++ source/applause_functions.py
@@ -2,34 +2,40 @@ from random import random
 from numpy import zeros, sqrt, nan_to_num
 from math import isnan
 
-
+#function that forces agents to go to state C
 def force_func(t, end):
     if t < end:
         return 1
     else:
         return 0
-        
+
+#f'(alpha); feedback based on the number of ppl already in state C; the more ppl in state C, the more ppl will clap
 def feedback_alpha(alpha, nC, population):
     return alpha * nC / (population - 1)
     
+#g'(beta)
 def feedback_beta(beta, nC, population):
     return 1 / (1 + beta * nC / (population - 1))
     
+#spatial-dependent feedack, '180 degrees'    
 def feedback_space(system,system_row, M):
     applause_state = [0]
     for i in range(system_row):
         applause_state.append(sum(system[i]))
     return nan_to_num(sum(applause_state)/(system_row*M))
 
+#quadratic equation    
 def quad_eq(x,y,z,sign):
     return (-y + sign * (sqrt((y ** 2) - 4 * x * z))) / 2 * x
 
+#do i still need this??!?!
 def frange(start, stop, step):
     i = start
     while i < stop:
         yield i
         i += step
-        
+   
+#creates 'network' of audience        
 def audience(N,M,C):
     agents = zeros((N,M))
     
@@ -41,7 +47,8 @@ def audience(N,M,C):
                 agents[i][j]=1
     
     return agents
-            
+
+#the actual simulator            
 def app_sim(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1):
     population = N * M
     AGENT = audience(N, M, C)
@@ -59,7 +66,8 @@ def app_sim(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1):
                     if random() <= bCtoS * feedback_beta(beta, nC, population):
                         AGENT[i,j] -= 1
     return graph
-    
+
+#graphs theoretical steady_state based on parameters    
 def steady_nC(aStoC, bCtoS, alpha, beta, population, sign):
     if beta == 0:
         if alpha == 0:
