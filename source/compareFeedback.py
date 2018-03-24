5c95ece36167ce2ae4f4ec883c24cd8441d8999c
Anton <antonvc@gmail.com>
2018-03-24 17:05:10 +0800

    add timer for simulations in compareFeedback

--- source/compareFeedback.py
+++ source/compareFeedback.py
@@ -12,32 +12,36 @@ created to compare the effects of the different feedbacks
 import applause_functions as app
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 N = 10
 M = 10
 population = N * M
-time = 500
+t = 500
 t_1 = 1
 C = 0
-aStoC = 0.8
-bCtoS = 0.1
+aStoC = 1
+bCtoS = 0.9
 alpha = 1
-beta = 5
+beta = 1
+
+start_time = time.time()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-sim1 = app.app_sim(aStoC, bCtoS, alpha, beta, N, M, C, time, t_1)
-sim2 = app.sim_space(aStoC, bCtoS, alpha, beta, N, M, C, time, t_1,M)
-sim3 = app.sim_space(aStoC, bCtoS, alpha, beta, N, M, C, time, t_1,0)
+sim1 = app.app_sim(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1)
+#sim2 = app.sim_space(aStoC, bCtoS, alpha, beta, N, M, C, time, t_1,M)
+#sim3 = app.sim_space(aStoC, bCtoS, alpha, beta, N, M, C, time, t_1,0)
 plt.plot(sim1, color='b')
-plt.plot(sim2, color='r')
-plt.plot(sim3, color='g')
+#plt.plot(sim2, color='r')
+#plt.plot(sim3, color='g')
 
-ax.text(5,101,'N = '+str(population), fontsize=15)
+ax.text(5,5,'N = '+str(population), fontsize=15)
 
 plt.title('$a = $'+str(aStoC)+' $b = $'+str(bCtoS)+r' $\alpha=$'+str(alpha)+r' $\beta=$'+str(beta),fontsize=20)
 plt.xlabel('Time',fontsize=18)
 plt.ylabel('State'+r' $n_{c}$',fontsize=18)
-plt.ylim(0,110)
+#plt.ylim(0,110)
 #plt.savefig('sim2.png')
 plt.show()
+print(str(time.time() - start_time) + ' seconds')
