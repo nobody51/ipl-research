e6d02df357b5cb3ca41ae309641f1cb6afd3f4d5
Anton <antonvc@gmail.com>
2018-04-06 20:16:28 +0800

    more thesis

--- source/compareFeedback.py
+++ source/compareFeedback.py
@@ -20,29 +20,33 @@ population = N * M
 t = 500
 t_1 = 1
 C = 0
-aStoC = 1
+aStoC = 0.7
 bCtoS = 0.8
-alpha = 1
-beta = 2
+alpha = 0.5
+beta = 4
 
 start_time = time.time()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 sim1 = app.app_sim(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1)
-#sim2 = app.sim_space(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1)
-#sim3 = app.sim_space(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1,0,1)
-plt.plot(sim1, color='b')
-#plt.plot(sim2, color='r')
-#plt.plot(sim3, color='g')
+sim2 = app.feed_space(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1,M,0)
+sim3 = app.feed_space(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1,0,1)
+sim4 = app.feed_space(aStoC, bCtoS, alpha, beta, N, M, C, t, t_1,0,0)
+plt.plot(sim1, color='b', label="FC")
+plt.plot(sim2, color='r', label="180deg")
+plt.plot(sim3, color='g', label="90deg")
+plt.plot(sim4, color='y', label="0deg")
+plt.legend(loc=4)
 
 ax.text(1,5,'N = '+str(population), fontsize=15)
 
 plt.title('$a = $'+str(aStoC)+' $b = $'+str(bCtoS)+r' $\alpha=$'+str(alpha)+r' $\beta=$'+str(beta),fontsize=20)
 plt.xlabel('Time',fontsize=18)
 plt.ylabel('State'+r' $n_{c}$',fontsize=18)
-#plt.ylim(0,110)
-#plt.savefig('sim2.png')
+plt.xlim(0,30)
+plt.ylim(0,110)
+plt.savefig('feedback_sim7.png')
 plt.show()
 #print(sim2)
-print(str(time.time() - start_time) + ' seconds')
+#print(str(time.time() - start_time) + ' seconds')
